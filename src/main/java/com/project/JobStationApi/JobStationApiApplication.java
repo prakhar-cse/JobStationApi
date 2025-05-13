0638a8ab1b8bfada25a9a2b519d19b6e160e7642
prakhar-cse <prakhar.cse.gbu@gmail.com>
2025-05-13 21:22:00 +0530

    init commit

---
+++ src/main/java/com/project/JobStationApi/JobStationApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.JobStationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobStationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobStationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/JobStationApi/JobStationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.JobStationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobStationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
