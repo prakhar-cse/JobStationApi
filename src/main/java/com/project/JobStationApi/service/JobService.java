eec93e2cae350416ab6f20801b9f8701139aa0ba
prakhar-cse <prakhar.cse.gbu@gmail.com>
2025-06-17 19:40:50 +0530

    added logs using Slf4j

--- src/main/java/com/project/JobStationApi/service/JobService.java
+++ src/main/java/com/project/JobStationApi/service/JobService.java
@@ -3,6 +3,9 @@ package com.project.JobStationApi.service;
 import com.project.JobStationApi.exception.JobNotFoundException;
 import com.project.JobStationApi.model.Job;
 import com.project.JobStationApi.repository.JobRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,23 +14,28 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class JobService {
 
+    private static final Logger log = LoggerFactory.getLogger(JobService.class);
     @Autowired
     JobRepository jobRepository;
 
     public List<Job> getAllJobs(){
+        log.info("---List of all jobs---");
         return jobRepository.findAll();
     }
 
     public Job getJobById(int jobId){
         if(jobId <= 0){
+            log.info("---{} is invalid---",jobId);
             throw new IllegalArgumentException("Job Id must be valid");
         }
         return jobRepository.findById(jobId).orElseThrow(()-> new JobNotFoundException("Job "+jobId+" is not present"));
     }
 
     public Job addJob(Job job){
+        log.info("---{} job is added---",job.getJobTitle());
         return jobRepository.save(job);
     }
 
@@ -38,6 +46,7 @@ public class JobService {
         if(! jobRepository.existsById(jobId)){
             throw new JobNotFoundException("Job "+jobId+" is not present");
         }
+        log.info("---jobid {} is deleted---",jobId);
         jobRepository.deleteById(jobId);
     }
 
@@ -52,6 +61,7 @@ public class JobService {
         JobObj.setJobTitle(newJobBody.getJobTitle());
         JobObj.setJobDescription(newJobBody.getJobDescription());
 
+        log.info("---jobId {} is updated---",job_id);
         jobRepository.save(JobObj);
         return JobObj;
     }
